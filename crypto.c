df310671e8ebaef28e9759712e9f8170671efeae
Ryuji Matsumura <ryuji@mgiken.com>
2011-11-14 13:30:10 +0900

    fix c macro

--- crypto.c
+++ crypto.c
@@ -3,19 +3,18 @@
 #include <openssl/md5.h>
 #include <openssl/sha.h>
 
-#define DEF(name, algo, len) \
+#define CRYPTO(ALGO)\
 char * \
-name(const char *d) \
+arc_##ALGO(const char *d) \
 { \
   int i; \
-  unsigned char md[len]; \
-  static char o[len*2]; \
+  unsigned char md[ALGO##_DIGEST_LENGTH]; \
+  static char o[ALGO##_DIGEST_LENGTH*2]; \
   memset(o, 0, sizeof(o)); \
-  algo((unsigned char*)d, strlen(d), md); \
-  for (i = 0; i < len; i++) sprintf(o+i*2, "%02x", md[i]); \
+  ALGO((unsigned char*)d, strlen(d), md); \
+  for (i = 0; i < ALGO##_DIGEST_LENGTH; i++) sprintf(o+i*2, "%02x", md[i]); \
   return o;\
 }
 
-
-DEF(arc_md5,    MD5,    MD5_DIGEST_LENGTH)
-DEF(arc_sha512, SHA512, SHA512_DIGEST_LENGTH)
+CRYPTO(MD5)
+CRYPTO(SHA512)
