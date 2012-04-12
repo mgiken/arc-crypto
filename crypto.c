e91dc018865bad53389efcd864f19d8e49a205f1
Ryuji Matsumura <ryuji@mgiken.com>
2012-04-13 00:20:21 +0900

    thread safe

--- crypto.c
+++ crypto.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+
 #include <openssl/md5.h>
 #include <openssl/sha.h>
 
 #define CRYPTO(ALGO)\
 char * \
-arc_##ALGO(const char *d) \
+arc_##ALGO(const char *d, char *o) \
 { \
   int i; \
   unsigned char md[ALGO##_DIGEST_LENGTH]; \
-  static char o[ALGO##_DIGEST_LENGTH*2]; \
-  memset(o, 0, sizeof(o)); \
   ALGO((unsigned char*)d, strlen(d), md); \
   for (i = 0; i < ALGO##_DIGEST_LENGTH; i++) sprintf(o+i*2, "%02x", md[i]); \
   return o;\
